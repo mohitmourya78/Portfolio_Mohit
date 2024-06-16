96de73fe4a4ce7a502d2ce17b71b52c1d2b13cbd
Mohit Mourya <mohitmourya7898@gmail.com>
2024-06-16 10:13:28 +0530

    second commit

--- src/Components/Rightside.jsx
+++ src/Components/Rightside.jsx
@@ -70,7 +70,7 @@ const Rightside = () => {
         <div className="flex rounded-lg w-full  bg-[url('/images/cover.jpeg')] bg-no-repeat bg-cover">
             <div className="flex justify-center md:rounded-ld overflow-hidden md:px-10 py-5 bg-blur backdrop-filter backdrop-blur-md w-full gap-2 md:gap-10">
                 <div className=''>
-                    <img className=' mohit-pfp object-cover border-4 border-white rounded-full shadow-md' src="./public/img/mohitpfp.jpg" alt="MohitPFP" />
+                    <img className=' mohit-pfp object-cover border-4 border-white rounded-full shadow-md' src="public/img/mohitpfp.jpg" alt="MohitPFP" />
                 </div>
                 <div className='flex md:flex-1 flex-col justify-center gap-4'>
                     <div className='flex justify-between items-center gap-5 '>
