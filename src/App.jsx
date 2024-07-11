11da83f2d45796c0c410296b43b19965cf38081d
Mohit Mourya <154969245+mohitmourya78@users.noreply.github.com>
2024-07-11 16:29:57 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Content from './Components/Content';
+
+import About from './Components/About';
+import Project from './Components/Project';
+import Contact from './Components/Contact';
+import Skills from './Components/Skills';
+
+
+function App() {
+  return (
+    
+    <div className="App ">
+      
+      <Header />
+
+      
+      <Content />
+
+      
+
+    </div>
+  );
+}
+
+export default App;
